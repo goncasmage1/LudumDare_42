8139fff0626d6540b333cb07ecca8bb1d6065d1b
Gonçalo Marques <goncalo.cool@gmail.com>
2018-08-11 18:53:04 +0100

    Add rock and poison cells

---
+++ Assets/Scripts/PoisonCell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+public class PoisonCell : MonoBehaviour {
+
+    public float Damage = 10f;
+    public float DamageInterval = 0.5f;
+    public float Lifespan = 10f;
+
+    private Health victim;
+
+	void Start () {
+        Destroy(gameObject, Lifespan);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerScript player = other.GetComponent<PlayerScript>();
+        if (player == null) return;
+
+        victim = player.GetComponent<Health>();
+        DoDamage();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        PlayerScript player = other.GetComponent<PlayerScript>();
+        if (player == null) return;
+
+        victim = null;
+        CancelInvoke("DoDamage");
+    }
+
+    void DoDamage()
+    {
+        if (victim == null) return;
+
+        victim.ReceiveDamage(Damage);
+        Invoke("DoDamage", DamageInterval);
+    }
+}
