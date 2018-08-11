4da231a9877fb1710cec4e9b81deaef58a8f9cf5
Gonçalo Marques <goncalo.cool@gmail.com>
2018-08-11 20:11:38 +0100

    Add health bar above enemies

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : Health {
 
     [HideInInspector]
     public AISpawner spawner;
+
+    public override void ReceiveDamage(float DamageAmount)
+    {
+        base.ReceiveDamage(DamageAmount);
+
+        transform.GetChild(0).GetChild(0).GetComponent<Image>().fillAmount = HealthAmount / MaxHealth;
+        Debug.Log("Damage!");
+    }
 
     public override void Die()
     {
