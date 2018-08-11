3f1b5c650539f752b7d93faf76d7e25a622c53a4
Gonçalo Marques <goncalo.cool@gmail.com>
2018-08-11 09:35:53 +0100

    Add some scripts and new scene

---
+++ Assets/Scripts/GridCell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class GridCell : MonoBehaviour {
+
+    [HideInInspector]
+    public bool isBlocking = false;
+
+    private BoxCollider2D boxCollider;
+
+    void Awake () {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+	
+    public void Block()
+    {
+        //Block collision
+    }
+
+    public void UnBlock()
+    {
+        //Unblock collision
+    }
+}
