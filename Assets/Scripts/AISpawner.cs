6a5615c55548dda201de3b5fe92f723493d1e6d9
Gonçalo Marques <goncalo.cool@gmail.com>
2018-08-11 11:49:11 +0100

    Spawn enemies over time

--- Assets/Scripts/AISpawner.cs
+++ Assets/Scripts/AISpawner.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class AISpawner : MonoBehaviour {
 
-	// Use this for initialization
+    public float EnemySpawnDistance = 10f;
+    public float SpawnInterval = 6f;
+    [Range(0.01f, 1f)]
+    public float SpawnIntervalReduction = 0.9f;
+
+    public Transform enemyPrefab = null;
+    private List<Enemy> enemies = new List<Enemy>();
+
 	void Start () {
-		
+        SpawnNewEnemy();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void SpawnNewEnemy()
+    {
+        Vector3 newLocation = new Vector3(1f, 0f, 0f);
+        newLocation = Quaternion.Euler(0, Random.Range(0f, 360f), 0) * newLocation;
+        Transform newTransform = Instantiate(enemyPrefab, newLocation * EnemySpawnDistance, Quaternion.identity);
+
+        Enemy newEnemy = newTransform.GetComponent<Enemy>();
+        if (newEnemy != null) enemies.Add(newEnemy);
+
+        SpawnInterval *= SpawnIntervalReduction;
+        Invoke("SpawnNewEnemy", SpawnInterval);
+    }
 }
