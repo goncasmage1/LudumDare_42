d2b8b59f507a481e152b5d5037a7fec246b9fcef
Gonçalo Marques <goncalo.cool@gmail.com>
2018-08-12 14:26:34 +0100

    Add Death Canvas

---
+++ Assets/Scripts/UI/RetryButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RetryButton : MonoBehaviour {
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(PlayGame);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Fred", LoadSceneMode.Single);
+    }
+}
